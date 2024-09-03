02e080eccd90258d752fd9cf009e0e04462204c0
Nemausa <tappan.billz@gmail.com>
2024-09-03 19:51:37 +0800

    fix(debug): Add python debug for Windows
    Signed-off-by: Nemausa <tappan.billz@gmail.com>

--- lua/config/dap_python.lua
+++ lua/config/dap_python.lua
@@ -4,22 +4,63 @@ local dapui = require('dapui')
 -- Configure nvim-dap-ui
 dapui.setup()
 
+local os_name = vim.loop.os_uname().sysname
 -- Set up Python adapter
-dap.adapters.python = {
-  type = 'executable',
-  command = '/usr/bin/python3.8',
-  args = { '-m', 'debugpy.adapter' },
-}
+if os_name == 'Linux' then
+    dap.adapters.python = {
+      type = 'executable',
+      command = '/usr/bin/python3.8',
+      args = { '-m', 'debugpy.adapter' },
+    }
 
--- Set up Python configuration
-dap.configurations.python = {
-  {
-    type = 'python',
-    request = 'launch',
-    name = 'Launch file',
-    program = '${file}',  -- Adjust this to the file you want to debug
-    pythonPath = function()
-      return '/usr/bin/python3.8'
-    end,
-  },
-}
+    -- Set up Python configuration
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'Launch file',
+        program = '${file}',  -- Adjust this to the file you want to debug
+        pythonPath = function()
+          return '/usr/bin/python3.8'
+        end,
+      },
+    }
+
+elseif os_name == 'Windows_NT' then
+    dap.adapters.python = {
+      type = 'executable',
+      command = 'C:\\Python312\\python.exe',
+      args = { '-m', 'debugpy.adapter' },
+    }
+
+    -- Set up Python configuration
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'Launch file',
+        program = '${file}',  -- Adjust this to the file you want to debug
+        pythonPath = function()
+          return 'C:\\Python312\\python.exe'
+        end,
+      },
+    }
+elseif os_name == "Darwin" then
+    dap.adapters.python = {
+      type = 'executable',
+      command = '/usr/bin/python3.8',
+      args = { '-m', 'debugpy.adapter' },
+    }
+    -- Set up Python configuration
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'Launch file',
+        program = '${file}',  -- Adjust this to the file you want to debug
+        pythonPath = function()
+          return '/usr/bin/python3.8'
+        end,
+      },
+    }
+end
