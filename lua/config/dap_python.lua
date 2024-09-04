5e4311098b9c4838334f657200dc112c2d1a260e
Nemausa <tappan.billz@gmail.com>
2024-09-04 14:02:27 +0800

    fix(debug): Debug Python in Ubuntu and Windows
    Signed-off-by: Nemausa <tappan.billz@gmail.com>

--- lua/config/dap_python.lua
+++ lua/config/dap_python.lua
@@ -1,69 +1,46 @@
 local dap = require('dap')
-local dapui = require('dapui')
-
--- Configure nvim-dap-ui
-dapui.setup()
-
 local os_name = vim.loop.os_uname().sysname
--- Set up Python adapter
-if os_name == 'Linux' then
-    dap.adapters.python = {
-      type = 'executable',
-      command = '/usr/bin/python3.8',
-      args = { '-m', 'debugpy.adapter' },
-    }
 
-    -- Set up Python configuration
-    dap.configurations.python = {
-      {
-        type = 'python',
-        request = 'launch',
-        name = 'Launch file',
-        program = '${file}',  -- Adjust this to the file you want to debug
-        pythonPath = function()
-          return '/usr/bin/python3.8'
-        end,
-        console = 'integratedTerminal',
-      },
-    }
+-- Helper function to find Python executable
+local function find_python()
+  local python_paths = {
+    "python3",         -- Default for Unix-like systems
+    "/usr/bin/python3",
+    "/usr/local/bin/python3",
+    "C:\\Python312\\python.exe", -- Default for Windows
+    "python",          -- Fallback for Unix-like systems
+  }
 
-elseif os_name == 'Windows_NT' then
-    dap.adapters.python = {
-      type = 'executable',
-      command = 'C:\\Python312\\python.exe',
-      args = { '-m', 'debugpy.adapter' },
-    }
+  -- Try to find a valid Python executable
+  for _, python_path in ipairs(python_paths) do
+    if vim.fn.executable(python_path) == 1 then
+      return python_path
+    end
+  end
 
-    -- Set up Python configuration
-    dap.configurations.python = {
-      {
-        type = 'python',
-        request = 'launch',
-        name = 'Launch file',
-        program = '${file}',  -- Adjust this to the file you want to debug
-        pythonPath = function()
-          return 'C:\\Python312\\python.exe'
-        end,
-        console = 'integratedTerminal',
-      },
-    }
-elseif os_name == "Darwin" then
-    dap.adapters.python = {
-      type = 'executable',
-      command = '/usr/bin/python3.8',
-      args = { '-m', 'debugpy.adapter' },
-    }
-    -- Set up Python configuration
-    dap.configurations.python = {
-      {
-        type = 'python',
-        request = 'launch',
-        name = 'Launch file',
-        program = '${file}',  -- Adjust this to the file you want to debug
-        pythonPath = function()
-          return '/usr/bin/python3.8'
-        end,
-        console = 'integratedTerminal',
-      },
-    }
+  -- Fallback if no Python executable was found
+  return "python"
 end
+
+local python_path = find_python()
+
+-- Set up Python adapter based on OS
+dap.adapters.python = {
+  type = 'executable',
+  command = python_path,
+  args = { '-m', 'debugpy.adapter' },
+}
+
+-- Set up Python configuration
+dap.configurations.python = {
+  {
+    type = 'python',
+    request = 'launch',
+    name = 'Launch file',
+    program = '${file}',  -- Adjust this to the file you want to debug
+    pythonPath = function()
+      return python_path
+    end,
+    console = 'integratedTerminal',
+  },
+}
