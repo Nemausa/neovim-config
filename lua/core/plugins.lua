6a2c9bd4c2cad02c3e3abac9fbd64ef01d533a37
Nemausa <tappanmorris@outlook.com>
2024-09-09 19:28:33 +0800

    fix(record): Delete folke/noice.nvim
    Signed-off-by: Nemausa <tappanmorris@outlook.com>

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -43,15 +43,4 @@ require('lazy').setup({
     end
   },
   'akinsho/toggleterm.nvim',
-  {
-    'folke/noice.nvim',
-    event = "VeryLazy",
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      'rcarriga/nvim-notify',
-    },
-    config = function()
-      require("noice").setup()
-    end
-  }
 })
