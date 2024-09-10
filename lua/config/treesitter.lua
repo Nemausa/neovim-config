fc8c3620c1216942234fa84f096544983caa6e0e
Nemausa <tappanmorris@outlook.com>
2024-09-10 13:17:37 +0800

    fix(treesitter): Disable highlight for >100kb
    Signed-off-by: Nemausa <tappanmorris@outlook.com>

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,24 +1,22 @@
-local function disable_treesitter_for_large_files(bufnr)
-  local max_filesize = 100 * 1024 -- 100 KB
-  local file = vim.api.nvim_buf_get_name(bufnr)
-  local ok, stats = pcall(vim.loop.fs_stat, file)
-
-  if ok and stats and stats.size < max_filesize then
-    vim.api.nvim_buf_set_option(bufnr, 'syntax', 'on')
-    require'nvim-treesitter.configs'.setup {
-      ensure_installed = { "c", "cpp", "python", "cmake", "markdown", "markdown_inline" },
-      highlight = {
-        enable = true,
-        disable = function(_, buf)
-          local buf_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
-          return buf_size < max_filesize
-        end,
-      },
-    }
-  else
-    vim.api.nvim_buf_set_option(bufnr, 'syntax', 'off')
-  end
+local disable_treesitter_for_large_files = function(bufnr)
+    local max_filesize = 100 * 1024 -- 100 KB
+    local stats = vim.loop.fs_stat(vim.api.nvim_buf_get_name(bufnr))
+    if stats and stats.size > max_filesize then
+        return true
+    else
+        return false
+    end
 end
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c", "cpp", "python", "cmake", "markdown", "markdown_inline" },
+    highlight = {
+        enable = true,
+        disable = function(lang, bufnr)
+            return disable_treesitter_for_large_files(bufnr)
+        end,
+    },
+}
 
 vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
