c9a6ec64d336e4b07c89c052a0c903b269231a9a
Nemausa <tappanmorris@outlook.com>
2024-10-27 14:17:41 +0800

    feat(qml): Add qml lsp
    Signed-off-by: Nemausa <tappanmorris@outlook.com>

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,14 +1,23 @@
-vim.api.nvim_set_keymap('n', '<leader>sc', ':lua SetTabOptions()<CR>', { noremap = true, silent = true })
-function SetTabOptions()
+vim.api.nvim_set_keymap('n', '<leader>t2', ':lua SetTab2()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>t4', ':lua SetTab4()<CR>', { noremap = true, silent = true })
+
+function SetTab2()
   vim.opt.softtabstop = 2
   vim.opt.shiftwidth = 2
   vim.opt.tabstop = 2
   vim.opt.expandtab = true
 end
 
+function SetTab4()
+  vim.opt.softtabstop = 4
+  vim.opt.shiftwidth = 4
+  vim.opt.tabstop = 4
+  vim.opt.expandtab = true
+end
+
 vim.cmd([[
-autocmd BufLeave,FocusLost * silent! wa!
-autocmd InsertLeave * silent! write
+  autocmd BufLeave,FocusLost * silent! wa!
+  autocmd InsertLeave * silent! write
 ]])
 
 --vim.g.NERDCommenterCommentString = '// %s'
@@ -18,7 +27,7 @@ vim.api.nvim_create_autocmd({"BufReadPost", "BufNewFile", "BufReadPre"}, {
   group = "SetTabOptionsGroup",
   pattern = {"*.c", "*.cpp", "*.h"},
   callback = function()
-      SetTabOptions()
+      SetTab2()
   end,
 })
 
