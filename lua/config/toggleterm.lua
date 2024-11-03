547d7e5e7403fe912d25ffc4679df33ee8673c8b
Nemausa <tappanmorris@outlook.com>
2024-11-03 23:10:29 +0800

    update
    Signed-off-by: Nemausa <tappanmorris@outlook.com>

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,16 +1,16 @@
-require('toggleterm').setup({
-    size = 10, -- 设置终端窗口的高度
-    open_mapping = [[<c-\>]], -- 设置快捷键打开/关闭终端
-    shade_terminals = true, -- 为终端窗口添加阴影效果
-    direction = 'horizontal', -- 终端的方向 ('horizontal' 表示水平，'vertical' 表示垂直)
-    close_on_exit = true, -- 终端关闭时自动关闭窗口
-    shell = vim.o.shell, -- 使用默认的 shell
-    float_opts = {
-        border = 'curved', -- 浮动窗口的边框样式（可选值有：'single', 'double', 'shadow', 'curved'）
-        width = 100,
-        height = 30,
-      },
-    on_open = function(term)
-        vim.cmd("startinsert")
-    end,
+require("toggleterm").setup({
+  size = function(term)
+    if term.direction == "horizontal" then
+      return vim.o.lines * 0.3  -- 将高度设置为窗口的 30%
+    else
+      return 20
+    end
+  end,
+  open_mapping = [[<c-\>]],
+  hide_numbers = true,
+  shade_terminals = true,
+  shading_factor = 2,
+  start_in_insert = true,
+  direction = "tab",
+  close_on_exit = true,
 })
