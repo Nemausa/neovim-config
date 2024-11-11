b8c4cde37ca6abf719d816e727b3c0b3a5b48770
Nemausa <tappanmorris@outlook.com>
2024-11-11 16:13:10 +0800

    update term

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -1,15 +1,14 @@
 require("toggleterm").setup({
   size = function(term)
     if term.direction == "horizontal" then
-      return vim.o.lines * 0.8  -- 将高度设置为窗口的 30%
+      return vim.o.lines * 1  -- 调整高度
     else
       return 20
     end
   end,
   open_mapping = [[<c-\>]],
-  hide_numbers = true,
-  shade_terminals = true,
-  shading_factor = 1,
+  hide_numbers = true,       -- 隐藏行号
+  shade_terminals = false,   -- 禁用终端着色，提高性能
   start_in_insert = true,
   direction = "horizontal",
   close_on_exit = true,
