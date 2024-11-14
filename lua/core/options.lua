53a88a17b8557a991736eaf9073b28f8c7ba6336
Nemausa <tappanmorris@outlook.com>
2024-11-14 12:12:02 +0800

    ll

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -18,21 +18,12 @@ vim.g.ale_enabled = 0
 vim.opt.termguicolors = true
 vim.opt.lazyredraw = true
 vim.g.python3_host_prog = find_python
--- vim.opt.hidden = true
--- vim.o.shada = "'100,<100,s10,h"
--- vim.opt.list = true
--- vim.opt.listchars:append("space:·")
--- vim.cmd [[highlight ExtraWhitespace guibg=lightgrey]]
 vim.o.fixendofline = false
 vim.o.clipboard = 'unnamedplus'
--- vim.o.shell = 'pwsh'
 vim.o.termguicolors = true -- 启用 24 位颜色
-vim.g.tagbar_width = 30  -- 设置侧边栏宽度
+-- vim.g.tagbar_width = 30  -- 设置侧边栏宽度
 vim.g.tagbar_position = 'left'  -- 设置侧边栏在左侧显示
-
--- vim.g.tagbar_width = 30           -- 设置宽度为 30 列
 vim.g.tagbar_autofocus = 1        -- 自动聚焦 tagbar 窗口
--- vim.g.tagbar_position = 'right'   -- 将 tagbar 窗口显示在右侧
 
 vim.cmd [[
   let g:gutentags_ctags_exclude = ['.git', 'node_modules', 'build']
