7c9ace6237777c7cf2032859d5f91850e74ab612
Nemausa <tappanmorris@outlook.com>
2024-11-20 20:33:35 +0800

    fix(telescope)
    Signed-off-by: Nemausa <tappanmorris@outlook.com>

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -5,28 +5,35 @@ require('telescope').setup {
     selection_caret = " ",
     entry_prefix = " ",
     sorting_strategy = "ascending",
-    file_ignore_patterns = { ".git/", "tags" },
+    file_ignore_patterns = { ".git/", "tags", "node_modules/", "__pycache__/", "*.lock" },
     layout_config = {
       horizontal = {
         prompt_position = "top",
         preview_width = 0.55,
       },
       width = 0.87,
-      height = 0.80,    
+      height = 0.80,
+      flex = {
+        flip_columns = 120,
+      },
     },
     mappings = {
       i = {
         ["<Esc>"] = actions.close,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-j>"] = actions.move_selection_next,
+      },
+      n = {
+        ["<C-c>"] = actions.close,
+        ["k"] = actions.move_selection_previous,
+        ["j"] = actions.move_selection_next,
       },
     },
   },
   extensions = {
     aerial = {
-      -- Set the width of the first two columns (the second
-      -- is relevant only when show_columns is set to 'both')
       col1_width = 4,
       col2_width = 30,
-      -- How to format the symbols
       format_symbol = function(symbol_path, filetype)
         if filetype == "json" or filetype == "yaml" then
           return table.concat(symbol_path, ".")
@@ -34,49 +41,27 @@ require('telescope').setup {
           return symbol_path[#symbol_path]
         end
       end,
-      -- Available modes: symbols, lines, both
       show_columns = "both",
     },
   },
 }
 
-vim.api.nvim_create_user_command(
-  'Ra',
-  function(opts)
-    local args = vim.split(opts.args, " ")
-    local old_string = args[1]
-    local new_string = args[2]
-    local match_type = args[3]
 
-    local search_pattern
-    if match_type == "exact" then
-      search_pattern = '\\V\\<' .. old_string .. '\\>'
-    else
-      search_pattern = old_string
-    end
 
-    vim.cmd('grep! ' .. old_string)
-    vim.cmd('copen')
-    vim.cmd('cdo s/' .. search_pattern .. '/' .. new_string .. '/g | update')
-    vim.cmd('cclose')
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<CR>', true, true, true), 'n', false)
-  end,
-  { nargs = '+' }
-)
-
---Telescope
 local map = vim.keymap.set
 map("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "telescope live grep" })
 map("n", "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "telescope find buffers" })
+map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
+
 -- map("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "telescope help page" })
 -- map("n", "<leader>ma", "<cmd>Telescope marks<CR>", { desc = "telescope find marks" })
 -- map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "telescope find oldfiles" })
-map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "telescope find in current buffer" })
 -- map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "telescope git commits" })
 -- map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "telescope git status" })
 -- map("n", "<leader>pt", "<cmd>Telescope terms<CR>", { desc = "telescope pick hidden term" })
 -- map("n", "<leader>th", "<cmd>Telescope themes<CR>", { desc = "telescope nvchad themes" })
-map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "telescope find files" })
+
 map(
   "n",
   "<leader>fa",
